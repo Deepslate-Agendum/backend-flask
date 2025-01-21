390798e16f13d1176dade9e865c62f9169574ea2
chwebb02 <chwebb2002@gmail.com>
2025-01-21 17:07:16 -0600

    Changed application to minimal RESTful API

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+_json_payload = "Hello from the Agendum REST API"
+
+@app.route("/", methods=["GET"])
 def hello_world():
-	return "<p>Hello, Agendum!</p>"
+	return jsonify({"message": _json_payload})
+
+if __name__ == "__main__":
+	app.run()
