017baf6c212ba94434894e403d364ac1e868d7d3
chwebb02 <chwebb2002@gmail.com>
2025-02-11 16:48:58 -0600

    AGENDUM-17 - Refine CRUD endpoints, pass requests to service layer, and stub service layer for CRUD operations.

---
+++ user/service.py
@@ -0,0 +1,11 @@
+def get(user_id: int = None) -> list | None:
+    return None
+
+def create(user_id: int, email: str = None, password: str = None) -> bool:
+    return False
+
+def update(user_id: int, email: str = None, password: str = None) -> bool:
+    return False
+
+def delete(user_id: int) -> bool:
+    return False
