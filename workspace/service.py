1691a774fa974c05dc65b15e7b0c7ebe6eb26619
chwebb02 <chwebb2002@gmail.com>
2025-02-11 23:13:39 -0600

    AGENDUM-34 - Fix broken conditional statements

--- workspace/service.py
+++ workspace/service.py
@@ -2,21 +2,21 @@ import workspace.dao as ws_dao
 
 def create(name: str, owner: str) -> int:
     """Create a new workspace."""
-    if ws_dao.get_by_name(name):
+    if ws_dao.get_by_name(name) is None:
         return -1
 
     return ws_dao.create(name, owner)
 
 def update(workspace_id: int, name: str = None, owner: str = None) -> bool:
     """Update a workspace."""
-    if not ws_dao.get_by_id(workspace_id):
+    if ws_dao.get_by_id(workspace_id) is None:
         return False
 
     return ws_dao.update(workspace_id, name, owner)
 
 def delete(workspace_id: int) -> bool:
     """Delete a workspace."""
-    if not ws_dao.get_by_id(workspace_id):
+    if ws_dao.get_by_id(workspace_id) is None:
         return False
 
     return ws_dao.delete(workspace_id)
