1e87c910b61d5db60564736b926db6c6589ef567
mbowman21 <sstraberry101@gmail.com>
2025-02-18 21:19:39 -0600

    commented out createTagField
    since it creates a circular code review with AGENDUM-56 and is a function used in AGENDUM-36 decided to comment it out for now.

--- db_helper.py
+++ db_helper.py
@@ -14,11 +14,13 @@ def connectToDatabase():
 def disconnectFromDatabase(connection):
     connection.close()
 
-# function that creates tag fields
-def createTagField(name):
-    tag_value_type = ValueType.objects(name = "Tag")
 
-    tag_field = Field(name = name, min_values = 1, max_values = 1, default_allowed_value = None, value_type=tag_value_type)
-    tag_field.save()
+# NOTE: this function is currently being worked on as it is used in AGENDUM-36 which is not complete.
+# # function that creates tag fields
+# def createTagField(name):
+#     tag_value_type = ValueType.objects(name = "Tag")
 
-    return tag_field
+#     tag_field = Field(name = name, min_values = 1, max_values = 1, default_allowed_value = None, value_type=tag_value_type)
+#     tag_field.save()
+
+#     return tag_field
