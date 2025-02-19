983aff2ff6de37724ad1ecab969ab8524c5cb15b
mbowman21 <sstraberry101@gmail.com>
2025-02-18 20:35:08 -0600

    database helper functions
    currently holds:
    - connecting to database
    - disconnecting from database
    - create tag field (which will be used by AGENDUM-36)

---
+++ db_helper.py
@@ -0,0 +1,24 @@
+from mongoengine import connect
+from DBClasses.classes import Field, ValueType
+
+# function that opens a connection to the database
+def connectToDatabase():
+    host = "mongodb://localhost:27017/mongodb"
+    connection = connect(host=host)
+    if connection == None:
+        raise Exception("Failed to connect to the database")
+    return connection
+
+
+# function that disconnects from the database
+def disconnectFromDatabase(connection):
+    connection.close()
+
+# function that creates tag fields
+def createTagField(name):
+    tag_value_type = ValueType.objects(name = "Tag")
+
+    tag_field = Field(name = name, min_values = 1, max_values = 1, default_allowed_value = None, value_type=tag_value_type)
+    tag_field.save()
+
+    return tag_field
