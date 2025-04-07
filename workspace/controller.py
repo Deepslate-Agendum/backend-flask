35b2fb97496bb7767937c7282f73bb614724c72d
Matt Marlow <marlowm314@gmail.com>
2025-04-06 23:36:49 -0500

    feat: added workspace validation

--- workspace/controller.py
+++ workspace/controller.py
@@ -3,24 +3,34 @@ import json
 from flask import Blueprint, jsonify, request
 import workspace.service as ws_service
 
+import mongoengine.errors as me_errors
+
+
 bp = Blueprint('workspace', __name__, url_prefix='/workspace')
 
 @bp.route('/create', methods=['POST'])
 def create():
-    name = request.json['name']
-    owner = request.json['owner']
-
+    try:
+        name = request.json['name']
+        owner = request.json['owner']
+    except KeyError as e:
+        return jsonify({"Request error": f"Missing {str(e)} in request body"}), 400
+    errors = ws_service.validate_create(name=name, owner=owner)
+    if (len(errors) > 0):
+        return jsonify({"Parameter error" : errors}), 400
     workspace_id = ws_service.create(name, owner)
     if workspace_id is not None:
         return jsonify({'workspace.id': workspace_id}), 200 # TODO: no periods in JSON keys
     else:
-        return jsonify({"error": "Workspace name already in use"}), 409
+        return jsonify({"error": "There was a problem creating a workspace"}), 500
 
 @bp.route('/', methods=['GET'])
 @bp.route('/<string:workspace_id>', methods=['GET'])
 def get(workspace_id: str = None):
+    # this seems like a terrible way to validate...
     workspaces = ws_service.get(workspace_id)
-
+    if isinstance(workspaces, me_errors.ValidationError):
+        return jsonify({'Parameter error': str(workspaces)}), 400
     # HACK: same deal as in user
     if isinstance(workspaces, list):
         workspaces_json = [json.loads(workspace.to_json()) for workspace in workspaces]
@@ -31,10 +41,15 @@ def get(workspace_id: str = None):
 
 @bp.route('/update', methods=['PATCH'])
 def update():
-    workspace_id = request.json['id']
-    name = request.json['name']
-    owner = request.json['owner']
-
+    try:
+        workspace_id = request.json['id']
+        name = request.json['name']
+        owner = request.json['owner']
+    except KeyError as e:
+        return jsonify({"Request error": f"Missing {str(e)} in request body"}), 400
+    errors = ws_service.validate_update(workspace_id, name, owner)
+    if len(errors) > 0:
+        return jsonify({"Parameter error": errors}), 400
     if ws_service.update(workspace_id, name, owner):
         return "Success", 200
     else:
@@ -42,8 +57,13 @@ def update():
 
 @bp.route('/delete', methods=['DELETE'])
 def delete():
-    workspace_id = request.json['id']
-
+    try:
+        workspace_id = request.json['id']
+    except KeyError as e:
+        return jsonify({"Request error": f"Missing {str(e)} in request body"}), 400
+    errors = ws_service.validate_delete(workspace_id)
+    if len(errors) > 0:
+        return jsonify({"Parameter error": errors}), 400
     if ws_service.delete(workspace_id):
         return "Success", 200
     else:
--- workspace/service.py
+++ workspace/service.py
@@ -1,26 +1,57 @@
 import workspace.dao as ws_dao
+import user.dao as user_dao
+
+import user.service as user_service
+
+import mongoengine.errors as me_errors
+from db_python_util.db_exceptions import EntityNotFoundException
 
 # TODO: need to fix type annotations here
 
+def validate_create(name: str, owner: str):
+    errors = []
+    if name == None:
+        errors.append("No name was provided for the workspace.")
+    if user_dao.get_by_id(owner) == None:
+        errors.append(f"The user ID {owner} does not correspond to a known user.")
+    if ws_dao.get_by_name(name) is not None:
+        errors.append(f"The name {name} is an already existing workspace.")
+    return errors
+
 def create(name: str, owner: str) -> int:
     """Create a new workspace."""
-    if ws_dao.get_by_name(name) is not None:
-        return -1
-
     return ws_dao.create(name, owner)
 
-def update(workspace_id: int, name: str = None, owner: str = None) -> bool:
-    """Update a workspace."""
-    if ws_dao.get_by_id(workspace_id) is None:
-        return False
+def validate_update(workspace_id: str, name: str = None, owner: str = None):
+    errors = []
+    ws_id_result = get(workspace_id)
+    if  ws_id_result is None:
+        errors.append(f"The provided workspace ID '{workspace_id}' does not correspond to an existing workspace.")
+    elif isinstance(ws_id_result, me_errors.ValidationError):
+        errors.append(f"The provided workspace ID '{workspace_id}' is not a valid workspace ID.")
+    if owner == None:
+        errors.append("There is no provided owner ID.")
+    user_id_result = user_service.get(owner)
+    if isinstance(user_id_result, EntityNotFoundException):
+        errors.append(f"The provided user ID '{owner}' is not a valid user ID.")
+    return errors
 
+def update(workspace_id: str, name: str = None, owner: str = None) -> bool:
+    """Update a workspace."""
     return ws_dao.update(workspace_id, name, owner)
 
-def delete(workspace_id: int) -> bool:
-    """Delete a workspace."""
-    if ws_dao.get_by_id(workspace_id) is None:
-        return False
 
+def validate_delete(workspace_id: str):
+    errors = []
+    ws_id_result = get(workspace_id)
+    if  ws_id_result is None:
+        errors.append(f"The provided workspace ID '{workspace_id}' does not correspond to an existing workspace.")
+    elif isinstance(ws_id_result, me_errors.ValidationError):
+        errors.append(f"The provided workspace ID '{workspace_id}' is not a valid workspace ID.")
+    return errors
+
+def delete(workspace_id: str) -> bool:
+    """Delete a workspace."""
     return ws_dao.delete(workspace_id)
 
 def get(workspace_id: int = None) -> list | None:
@@ -28,4 +59,9 @@ def get(workspace_id: int = None) -> list | None:
     if workspace_id is None:
         return ws_dao.get_all()
     else:
-        return ws_dao.get_by_id(workspace_id)
+        try:
+            return ws_dao.get_by_id(workspace_id)
+        except me_errors.ValidationError as e:
+            return e
+        except EntityNotFoundException as e:
+            return e
