734f28463450ddd2a267a33b8d09dfa0b39c9e99
chwebb02 <chwebb2002@gmail.com>
2025-04-08 17:15:24 -0500

    token service bug hotfix

--- user_token/service.py
+++ user_token/service.py
@@ -1,8 +1,8 @@
 import secrets
-from datetime import datetime
+from datetime import datetime, timedelta
 
 USER_TOKEN_LENGTH = 64
-TOKEN_EXPIRY_DURATION = datetime(minute=15)
+TOKEN_EXPIRY_DURATION = timedelta(hours=1)
 
 # This is hacky, but it seems to be the simplest and memory isn't
 # a concern for us.
@@ -17,15 +17,15 @@ class UserData:
     
 def generate_token() -> str:
     """Do not use, internal function for user token generation"""
-    return secrets.token_bytes(USER_TOKEN_LENGTH)
+    return str(secrets.token_bytes(USER_TOKEN_LENGTH))
 
 def register_new_token(user_id: str) -> str:
     """Register a token to a user who has been authenticated"""
-    if user_to_token_map[user_id] is not None:
+    if user_to_token_map.get(user_id) is not None:
         raise Exception("This user is already registered to a token")
     
     token = generate_token()
-    while token_to_user_data_map[token] is not None:
+    while token_to_user_data_map.get(token) is not None:
         token = generate_token()
     
     user_to_token_map[user_id] = token
@@ -37,22 +37,22 @@ def authenticate_token(token: str) -> str:
     """Get the user that a token is assigned to, after validating that the token is still valid"""
     refresh_token(token)
 
-    return token_to_user_data_map[token].user_id
+    return token_to_user_data_map.get(token).user_id
 
 def refresh_token(token: str) -> None:
     """Refresh a token, pushing expiry time to 15 minutes from now"""
-    if token_to_user_data_map[token] is None:
+    if token_to_user_data_map.get(token) is None:
         raise Exception("Token does not exist")
     
-    if token_to_user_data_map[token].last_used_timestamp + TOKEN_EXPIRY_DURATION < datetime.now():
+    if token_to_user_data_map.get(token).last_used_timestamp + TOKEN_EXPIRY_DURATION < datetime.now():
         release_token(token)
         raise Exception("Token has expired and has now been deleted")
     
-    token_to_user_data_map[token].last_activity_timestamp = datetime.now()
+    token_to_user_data_map.get(token).last_activity_timestamp = datetime.now()
 
 def release_token(token: str) -> None:
     """Release a token, effectively logging out a user"""
-    if token_to_user_data_map[token] is None:
+    if token_to_user_data_map.get(token) is None:
         raise Exception("Token does not exist")
 
     user = token_to_user_data_map.pop(token)
