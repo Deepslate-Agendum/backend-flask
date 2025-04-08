85a570531c2d88d2c4ff2ab6e049b910651cfda0
creallfluharty <creallfluharty@gmail.com>
2025-04-08 12:40:15 -0500

    remove dependency update

--- dependency/service.py
+++ dependency/service.py
@@ -6,9 +6,6 @@ def create_dependency(dependee_id: str, dependent_id: str, manner: str):
     dependency = dao.create_dependency(dependee_id, dependent_id, manner)
     return dependency
 
-# def update_dependency(dependency_id: str, dependee_id: str, dependent_id: str, manner: str):
-#     return dao.udpate_dependency(dependency_id=dependency_id, dependee_id=dependee_id, dependent_id=dependent_id, manner=manner)
-
 def get_all_dependencies():
     pass
 
