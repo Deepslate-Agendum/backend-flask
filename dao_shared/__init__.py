e62200704c5579b005326e2ebb4e4814952809fc
creallfluharty <creallfluharty@gmail.com>
2025-04-10 22:33:22 -0500

    remove workspace TODO + better handle missing ids

--- dao_shared/__init__.py
+++ dao_shared/__init__.py
@@ -1,11 +1,11 @@
 from typing import List
 from bson import ObjectId
 from mongoengine.errors import ValidationError
+from bson.errors import InvalidId
 
 from db_python_util.db_helper import ConnectionManager
 from db_python_util.db_exceptions import EntityNotFoundException
 
-# TODO: require workspace id
 @ConnectionManager.requires_connection
 def get_document_by_id(collection: type, id: str, allow_none: bool=False):
     try:
@@ -21,13 +21,17 @@ def get_document_by_id(collection: type, id: str, allow_none: bool=False):
 
     return document
 
-# TODO: require workspace id
 @ConnectionManager.requires_connection
 def get_documents_by_ids(collection: type, ids: List[str], require_all: bool=False):
     try:
-        oids = [ObjectId(id) for id in ids]
-    except ValidationError: # An ObjectId "must be a 12-byte input or a 24-character hex string", but front-end shouldn't need to care about that
         oids = []
+        for id in ids:
+            oids.append(ObjectId(id))
+    except InvalidId: # An ObjectId "must be a 12-byte input or a 24-character hex string", but front-end shouldn't need to care about that
+        raise EntityNotFoundException(
+            collection,
+            f"No {collection.__name__} with id '{id}' could be found"
+        )
 
     documents = collection.objects(id__in=oids)
 
@@ -35,7 +39,7 @@ def get_documents_by_ids(collection: type, ids: List[str], require_all: bool=Fal
         missing_ids = set(oids).difference(document.pk for document in documents)
         raise EntityNotFoundException(
             collection,
-            f"Could not find {collection.__name__}s with ids [{missing_ids.join(', ')}]"
+            f"No {collection.__name__} with id '{next(iter(missing_ids))}' could be found"
         )
 
     return documents
