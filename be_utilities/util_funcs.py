69c4737c9bb6d3a6594cc4d9abed4b8b67c50e97
Matt Marlow <marlowm314@gmail.com>
2025-04-13 20:56:08 -0500

    feat: implemented common tuple for known exceptions

--- be_utilities/util_funcs.py
+++ be_utilities/util_funcs.py
@@ -1,4 +1,7 @@
 from .controller_exceptions import MissingArgumentException
+from .service_exceptions import ServiceException
+from .controller_exceptions import ControllerException
+from db_python_util.db_exceptions import DBException
 def get_param_from_body(request, param_name, default = None):
     try:
         if default == None:
@@ -12,3 +15,5 @@ def get_param_from_url(request, param_name):
         return request.args[param_name]
     except KeyError:
         raise MissingArgumentException(f"Missing {param_name} in request URL arguments.")
+
+KNOWN_EXCEPTIONS = (ControllerException, DBException, ServiceException)
