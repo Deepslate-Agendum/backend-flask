0d43d8fccd236ffe4db2b75e04832dbfa9498a2a
Matt Marlow <marlowm314@gmail.com>
2025-04-14 13:03:43 -0500

    fix: removed redundant controller exception

--- be_utilities/controller_exceptions.py
+++
@@ -1,5 +0,0 @@
-class ControllerException(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class MissingArgumentException(ControllerException):
-    pass
--- be_utilities/util_funcs.py
+++ be_utilities/util_funcs.py
@@ -1,6 +1,4 @@
-from .controller_exceptions import MissingArgumentException
 from .service_exceptions import ServiceException
-from .controller_exceptions import ControllerException
 from db_python_util.db_exceptions import DBException
 def get_param_from_body(request, param_name, default = None):
     try:
@@ -9,11 +7,11 @@ def get_param_from_body(request, param_name, default = None):
         else:
             return request.json.get(param_name, default)
     except KeyError:
-        raise MissingArgumentException(f"Missing {param_name} in request JSON body.")
+        raise KeyError(f"Missing {param_name} in request JSON body.")
 def get_param_from_url(request, param_name):
     try:
         return request.args[param_name]
     except KeyError:
-        raise MissingArgumentException(f"Missing {param_name} in request URL arguments.")
+        raise KeyError(f"Missing {param_name} in request URL arguments.")
 
-KNOWN_EXCEPTIONS = (ControllerException, DBException, ServiceException)
+KNOWN_EXCEPTIONS = (KeyError, DBException, ServiceException)
