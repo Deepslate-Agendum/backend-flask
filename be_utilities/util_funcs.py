ebd858e39dc4ee181f99395f7b34249d05608360
Matt Marlow <marlowm314@gmail.com>
2025-04-14 20:53:37 -0500

    fix: catch BadRequest

--- be_utilities/util_funcs.py
+++ be_utilities/util_funcs.py
@@ -1,5 +1,6 @@
 from .service_exceptions import ServiceException
 from db_python_util.db_exceptions import DBException
+from werkzeug.exceptions import BadRequest
 def get_param_from_body(request, param_name, default = None):
     try:
         if default == None:
@@ -8,10 +9,12 @@ def get_param_from_body(request, param_name, default = None):
             return request.json.get(param_name, default)
     except KeyError:
         raise KeyError(f"Missing {param_name} in request JSON body.")
+    except BadRequest:
+        raise BadRequest(f"{param_name} is malformed in the request JSON body.")
 def get_param_from_url(request, param_name):
     try:
         return request.args[param_name]
     except KeyError:
         raise KeyError(f"Missing {param_name} in request URL arguments.")
 
-KNOWN_EXCEPTIONS = (KeyError, DBException, ServiceException)
+KNOWN_EXCEPTIONS = (KeyError, BadRequest, DBException, ServiceException)
