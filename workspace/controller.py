940e597dea0b3c9357a762187cb42580b922ed76
mbowman21 <sstraberry101@gmail.com>
2025-04-29 10:33:50 -0500

    Update controller.py

--- workspace/controller.py
+++ workspace/controller.py
@@ -21,7 +21,7 @@ def create():
 @workspaces_bp.route('/', methods=['GET'])
 @workspace_bp.route('/', methods=['GET'])
 def get(workspace_id: str = None):
-    user_token = request.get('Authorization').split()[1]
+    user_token = request.headers.get('Authorization').split()[1]
 
     workspaces = ws_service.get(workspace_id, user_token)
 
